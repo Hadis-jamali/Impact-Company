2eba96385f635d56a466f4cdcfc6c21c902b50cc
Hadis Jamali <hadisjamali95@gmail.com>
2023-07-13 19:07:32 +0200

    add section

--- js/project.js
+++ js/project.js
@@ -3,3 +3,18 @@ caret.addEventListener("click", () => {
   caret.lastElementChild.classList.toggle("bi-caret-down");
   caret.lastElementChild.classList.toggle("bi-caret-up");
 });
+
+var spriteWidth = 400;
+var steps = 1;
+
+var currentStep = 0;
+var currentPos = 0;
+var speed = 800;
+
+function animate() {
+  currentStep = (currentStep + 1) % steps;
+  currentPos = -currentStep * spriteWidth + "px";
+  document.querySelector(".animation").style.backgroundPosition = currentPos + " top";
+}
+
+setInterval(animate, speed);
